e6a27c4ed1cd31f41cbd5f5325c76a2234d64b9c
jbernal <miw@etsisi.upm.es>
2019-02-03 18:32:16 +0100

    add heroku

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+
+
+//Set static Folder
+app.use(express.static(path.join(__dirname + '/dist/demo')));
+
+//Index routing
+app.get('/*', (req, res)=>{
+  res.sendFile(path.join(__dirname + '/dist/demo/index.html'));
+});
+
+app.listen(process.env.PORT || 8080, ()=>{
+  console.log('Server started on port', port);
+});
